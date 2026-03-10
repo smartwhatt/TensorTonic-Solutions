6d68fc6b37cc02198ac1a108689fdcbf48e89a96
Smart Wattanapornmongkol <53175779+smartwhatt@users.noreply.github.com>
2026-03-10 10:32:30 +0700

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -4,13 +4,11 @@ def entropy_node(y):
     """
     Compute entropy for a single node using stable logarithms.
     """
+    # Write code here
     if len(y) == 0:
         return 0.0
     
     y = np.array(y)
-    _, counts = np.unique(y, return_counts=True)
-    
-    p = counts / counts.sum()
-    p = p[p > 0]  # remove zero probabilities
-    
-    return -np.sum(p * np.log2(p))
+    unique_values, counts = np.unique(y, return_counts=True)
+    dist = counts/y.shape[0]
+    return -sum(np.where(dist == 0, 0, dist*np.log2(dist)))
