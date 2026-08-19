27489f382cfdfc377638e04c22c387096c1eecff
Smart Wattanapornmongkol <53175779+smartwhatt@users.noreply.github.com>
2026-08-19 14:09:14 +0700

    Solve perplexity-computation

--- perplexity-computation/perplexity-computation.py
+++ perplexity-computation/perplexity-computation.py
@@ -6,7 +6,7 @@ def perplexity(prob_distributions, actual_tokens):
     # Write code here
     prob_distributions = np.array(prob_distributions)
     actual_tokens = np.array(actual_tokens)
-    p = np.array([prob_distributions[t][actual_tokens[t]] for t in range(len(actual_tokens))])
+    p = prob_distributions[np.arange(len(actual_tokens)), actual_tokens]
 
     H = -1/len(actual_tokens) * np.log(p).sum()
     return np.exp(H)
